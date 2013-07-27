5af33ac8517c2809e96d30e88e6261ebab5c1353
Daniel Haskin <djhaskin987@gmail.com>
2013-07-27 13:38:51 -0600

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ranked-set "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Retreive and cut elements from a set based on rank."
+  :url "http://djhaskin987.blogspot.com/2013/07/the-rankedset-optimizations-best-friend.html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
